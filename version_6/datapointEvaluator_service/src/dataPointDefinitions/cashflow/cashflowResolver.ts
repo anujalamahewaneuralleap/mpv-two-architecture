67c75145163a5377cf7a4ce697de0431da0ecf9c
anujalamahewaneuralleap <anuja.lamahewa@neuralleap.ai>
2024-01-30 06:48:52 +0530

    added parallel running logic

--- version_6/datapointEvaluator_service/src/dataPointDefinitions/cashflow/cashflowResolver.ts
+++ version_6/datapointEvaluator_service/src/dataPointDefinitions/cashflow/cashflowResolver.ts
@@ -6,6 +6,7 @@ import { calculateNetIncome } from "./03_netIncome/netIncomeService";
 import { calculateNetOtherIncome } from "./13_netOtherIncome/netOtherIncomeService";
 import { calculateOrdinaryIncome } from "./07_netOrdinaryIncome/netOrdinaryIncomeService";
 import { _MathOperationInput, _MathOperationResult } from "./cashflowDefinitionInterfaceService";
+import { executeParallelTasks } from "./parallelTaskService";
 
 let result = 0; // this is the final calculated value
 
@@ -48,11 +49,11 @@ export const cashflowResolver = async (request: FastifyRequest, reply: FastifyRe
 			const doceasyResponseJson = JSON.parse(response.data.data.doceasy_json_string);
 
 			/*
-      let's store individual values to make the logic simple
-      the first 2 period_ordinary_income, period_other_income
-      we get the values from doceasy but to test out our current logic
-      we will do the calculation
-      */
+			let's store individual values to make the logic simple
+			the first 2 period_ordinary_income, period_other_income
+			we get the values from doceasy but to test out our current logic
+			we will do the calculation
+			*/
 			let period_ordinary_income = doceasyResponseJson.data.period_1_net_ordinary_income[0];
 			let period_other_income = doceasyResponseJson.data.period_1_other_income_total[0];
 
@@ -63,27 +64,35 @@ export const cashflowResolver = async (request: FastifyRequest, reply: FastifyRe
 			let period_total_other_expences = doceasyResponseJson.data.period_1_net_other_expenses[0];
 
 			/*
-      Calculation is as follow:
-      Income = Ordinary Income (period_1_net_ordinary_income) − Other Income (period_1_other_income_total)
-      Ordinary Income = Gross Income (period_1_gross_income) − Operating Expenses (period_1_operating_expenses_total)
-      Other Income = Total Other Income (period_1_net_other_income) − Total Other Expenses (NOT FOUND in doceasy used period_1_net_other_expenses)
-      
-      we have to go in order
-      */
+			Calculation is as follow:
+			Income = Ordinary Income (period_1_net_ordinary_income) − Other Income (period_1_other_income_total)
+			Ordinary Income = Gross Income (period_1_gross_income) − Operating Expenses (period_1_operating_expenses_total)
+			Other Income = Total Other Income (period_1_net_other_income) − Total Other Expenses (NOT FOUND in doceasy used period_1_net_other_expenses)
+			
+			we have to go in order
+			*/
 
-			// first calculate Other Income
-			let otherIncome: _MathOperationResult = calculateNetOtherIncome(Number(period_total_other_income), Number(period_total_other_expences));
-			// then Ordinary Income
-			let ordinaryIncome: _MathOperationResult = calculateOrdinaryIncome(Number(period_gross_income), Number(period_operating_expences));
-			// finally we can calcuate the total by the above 2 values
-			let netIncome = calculateNetIncome(Number(ordinaryIncome.result), Number(otherIncome.result));
+			// // first calculate Other Income
+			// let otherIncome: _MathOperationResult = calculateNetOtherIncome(Number(period_total_other_income), Number(period_total_other_expences));
+			// // then Ordinary Income
+			// let ordinaryIncome: _MathOperationResult = calculateOrdinaryIncome(Number(period_gross_income), Number(period_operating_expences));
+			// // finally we can calcuate the total by the above 2 values
+			// let netIncome = calculateNetIncome(Number(ordinaryIncome.result), Number(otherIncome.result));
 
-			// console log for debugging
-			console.log("Net Other Income:", otherIncome.result);
-			console.log("Net Ordinary Income:", ordinaryIncome.result);
-			console.log("Net Income:", netIncome.result);
+			// // console log for debugging
+			// console.log("Net Other Income:", otherIncome.result);
+			// console.log("Net Ordinary Income:", ordinaryIncome.result);
+			// console.log("Net Income:", netIncome.result);
 
-			result = netIncome.result; // setting it to result
+			// result = netIncome.result; // setting it to result
+
+			executeParallelTasks
+			(
+				period_total_other_income, 
+				period_total_other_expences, 
+				period_gross_income, 
+				period_operating_expences
+			);
 
 			//result = response.data; // this is only for debugging purpose
 		} // end of else
